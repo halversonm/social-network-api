14fc7d8617a10eefb4b6441a994d5db7dbc8e7c4
mh <maxhalverson@live.com>
2023-09-12 19:56:59 -0500

    added userRoutes

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,4 +1,13 @@
 const router = require('express').Router();
+const {
+    getUsers,
+    getSingleUser,
+    createUser,
+    updateUser,
+    deleteUser,
+} = require('../../controllers/userController');
 
+router.route('/').get(getUsers).post(createUser);
+router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
 
 module.exports = router;
